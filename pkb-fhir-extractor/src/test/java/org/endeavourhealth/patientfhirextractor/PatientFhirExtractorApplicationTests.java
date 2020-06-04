977f7a150a7fbd606263cef87f22ae3881cc5348
dhanush <dhanushthakkalapally@gmail.com>
2020-06-05 00:40:06 +0100

    Nothing changed

--- pkb-fhir-extractor/src/test/java/org/endeavourhealth/patientfhirextractor/PatientFhirExtractorApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package org.endeavourhealth.patientfhirextractor;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PatientFhirExtractorApplicationTests {
-
-
-}
